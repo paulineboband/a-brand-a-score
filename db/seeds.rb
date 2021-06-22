3f3580886f6bec1da97096956b310999cb50eb56
Oceane Lucas <oceane.lucas@edhec.com>
2021-06-22 15:36:45 +0200

    new gem nokogiri

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+require 'nokogiri'
+
+brand = 'nike'
+url = "https://fr.igraal.com/codes-promo/#{brand}/code-promotionnel"
+
+html_file = URI.open(url, "User-Agent" => "salut").read
+html_doc = Nokogiri::HTML(html_file)
+
+html_doc.search('merchant-header__title title title--sec bold-text color-black').each do |element|
+  puts element.text.strip
+end
