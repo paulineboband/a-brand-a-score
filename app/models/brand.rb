8de19b90d0927ba7b01592a0fc0320e4f6561cd8
Oceane Lucas <oceane.lucas@edhec.com>
2021-06-22 12:43:23 +0200

    has_many/belongs_to

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,6 @@
 class Brand < ApplicationRecord
+  has_many :favorites
+  has_many :reviews
+  has_many :brand_categories
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :brand
+
+  has_many :tag_reviews
+  has_many :votes
 end
