d0ea12d2b417b24a8f8233a12d3b23bcd6a74e41
Oceane Lucas <oceane.lucas@edhec.com>
2021-06-23 12:13:40 +0200

    modal review + review added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,16 @@
 class ReviewsController < ApplicationController
-  def new
-    @brand = Brand.find(params[:brand_id])
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     @brand = Brand.find(params[:brand_id])
     @review.brand = @brand
-    @review.save
-    redirect_to brand_path(@brand)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to brand_path(@brand)
+    else
+      redirect_to brand_path(@brand), notice: "Error: the review did not save"
+      #render_to_string(partial: "/shared/modalreview", locals: { brand: @brand, review: @review })
+    end
   end
 
   private
