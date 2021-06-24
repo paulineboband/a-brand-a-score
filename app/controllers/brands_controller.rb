0f29c039f7569601b2d47d6fd3bbcf35145f46c2
Oceane Lucas <oceane.lucas@edhec.com>
2021-06-24 12:04:34 +0200

    consumer score

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -5,20 +5,22 @@ class BrandsController < ApplicationController
     else
       @brands = Brand.all
     end
-    
-   @brands.each do |brand|
+
+    @brands.each do |brand|
       brand.overall_score = (brand.environmental_score.to_f + brand.social_score.to_f + brand.quality_score.to_f) / 3
-   end
+    end
   end
 
   def show
     @brand = Brand.find(params[:id])
     @brand.overall_score = (@brand.environmental_score.to_f + @brand.social_score.to_f + @brand.quality_score.to_f) / 3
 
-
     @toggle = current_user.favorites.select { |favorite| favorite.brand_id == @brand.id }
 
     @review = Review.new
 
+    sum = 0
+    @brand.reviews.each { |review| sum += review.score }
+    @consumer_score = sum / @brand.reviews.size.to_f
   end
 end
