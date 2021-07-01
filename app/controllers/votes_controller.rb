3aa45f05403e6531b52c73679fc6a62b6bdc1f42
Oceane Lucas <oceane.lucas@edhec.com>
2021-07-01 17:22:30 +0200

    redirect corrected

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,13 +7,16 @@ class VotesController < ApplicationController
     @brand = @review.brand
     if @vote.save
       flash[:notice] = "+1 vote added to the review."
-      redirect_to controller: 'brands', action: 'show', id: @brand.id, review_tab: 'true'
+      # redirect_to controller: 'brands', action: 'show', id: @brand.id, review_tab: 'true'
+      redirect_to brand_path(@brand)
     elsif @vote.present?
       flash[:alert] = "You have already voted."
-      redirect_to controller: 'brands', action: 'show', id: @brand.id, review_tab: 'true'
+      #redirect_to controller: 'brands', action: 'show', id: @brand.id, review_tab: 'true'
+      redirect_to brand_path(@brand)
     else
       flash[:alert] = "Error: the vote has not been added."
-      redirect_to controller: 'brands', action: 'show', id: @brand.id, review_tab: 'true'
+      # redirect_to controller: 'brands', action: 'show', id: @brand.id, review_tab: 'true'
+      redirect_to brand_path(@brand)
     end
   end
 
